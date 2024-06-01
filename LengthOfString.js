4952b864687982b130d827c0fbc3a77c57991b27
Kumar-Awadhesh <140824899+Kumar-Awadhesh@users.noreply.github.com>
2024-06-01 20:55:57 +0530

    Add files via upload

---
+++ LengthOfString.js
@@ -0,0 +1,6 @@
+const array=["apple", "windows", "ubuntu"];
+// iteration over array till it's length.
+array.forEach((el)=>{
+    //printing the length of each element of the array.
+    console.log(el.length)
+})
